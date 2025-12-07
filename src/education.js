d2ae948a40855f547f94cb3fb151f540a6bb3fe1
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-12-06 23:34:14 -0600

    Finally got card layout to work for education resources

--- src/education.js
+++ src/education.js
@@ -20,7 +20,13 @@ const resources = [
 ];
 
 // Populate page with education resources
-for (let i = 0; i < resources.length; i++) {
-    document.querySelector(`section.education`).innerHTML = `
-    `;
-}
+// for (let i = 0; i < resources.length; i++) {
+//     document.querySelector(`section.education`).innerHTML = `
+//     `;
+// }
+
+// White with 50% opacity
+const overlay = 'rgba(255, 255, 255, 0.75)';
+
+// background: imageURL repeat verticalPosition horizontalPosition / size
+document.querySelector('div.resource').style.setProperty('background', `linear-gradient(${overlay}, ${overlay}), url("assets/img/sites/education/0.png") no-repeat top center / cover`, 'important');
