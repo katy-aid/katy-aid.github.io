dc1998cb1d1a7e5b788d4a2521d7deed49c52a6c
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-11 16:21:27 +0000

    Added worship places to database for cultural resources

--- src/cultural.js
+++ src/cultural.js
@@ -68,3 +68,70 @@ const markets = [
         description: 'Supermarket chain known for a vast selection of East and Southeast Asian groceries, fresh produce, live seafood, meats, bakery items, prepared foods, and specialty products'
     }
 ];
+const worship = [
+    {
+        name: 'The Fellowship',
+        url: '',
+        description: 'A non-denominational church with multiple Sunday services and programs for kids and students'
+    },
+    {
+        name: 'Cinco Ranch Church of Christ',
+        url: '',
+        description: 'Offers Bible classes and worship with no dress code, emphasizing simple, New Testament Christianity'
+    },
+    {
+        name: 'Grace Fellowship Church',
+        url: '',
+        description: 'A large church with a strong focus on youth ministries'
+    },
+    {
+        name: 'Vien Thong Tu',
+        url: '',
+        description: 'Vietnamese Buddhist temple known for its beautiful architecture, peaceful atmosphere, and vibrant community; also offers a popular vegetarian restaurant on weekends, serving delicious vegan food and fostering spiritual growth through meditation and cultural event'
+    },
+    {
+        name: 'Lien Hoa Temple',
+        url: '',
+        description: 'Vietnamese Buddhist centers that serve as spiritual hubs for meditation, cultural events, and charitable work, offering teachings in both Vietnamese and English, and fostering community with practices like mindfulness, compassionate living, and shared vegetarian meals'
+    },
+    {
+        name: 'Texas Teo Chew Temple',
+        url: '',
+        description: 'Vietnamese Buddhist temple known for its colorful traditional Chinese architecture, serene atmosphere, and guardian statues'
+    },
+    {
+        name: 'Masjid Aqsa (Katy Islamic Center)',
+        url: '',
+        description: 'Central spot for daily prayers (Salat), Friday prayers, a Weekend Islamic School, and Quran classes'
+    },
+    {
+        name: 'West Katy Islamic Center',
+        url: '',
+        description: 'Offering daily prayers and community activities and organized for religious, chariable, and educational purposes to serve the Muslim community'
+    },
+    {
+        name: 'Al-Huda Islamic Center',
+        url: '',
+        description: 'Focuses on religious guidance, education, youth development, and community services for Muslims and non-Muslims in the Katy area'
+    },
+    {
+        name: 'Gayatri Temple',
+        url: '',
+        description: 'First permanent Hindu temple in Katy that serves as a central hub for prayer, classes, and gatherings'
+    },
+    {
+        name: 'Katy Hindu Community',
+        url: '',
+        description: 'An organization that hosts events like the annual Sri Sita Rama Kalyana Mahotsavam, often in collaboration with temples'
+    },
+    {
+        name: 'Shivavishnu Mandir',
+        url: '',
+        description: 'A Hindu temple serving the local community with daily pujas, major festival celebrations (like Diwali, Navaratri), and spiritual activities for youth and seniors'
+    },
+    {
+        name: 'Temple Sinai',
+        url: '',
+        description: 'A reform synagogue located near Katy, known for being warm, friendly inclusive, and welcoming to interfaith families, with services and activities for all ages'
+    }
+]
