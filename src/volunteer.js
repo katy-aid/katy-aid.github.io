476aca0c8076059cb8df59c773ceefa5a8616262
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-11 21:48:00 +0000

    Started adding volunteer opportunities to database

---
+++ src/volunteer.js
@@ -0,0 +1,25 @@
+/*** VOLUNTEER OPPORTUNITIES ***/
+
+// Database for volunteer opportunities
+const opportunities = [
+    {
+        name: 'Ballard House',
+        url: '',
+        description: 'Offers no-cost temporary housing for individuals and caregivers undergoing treatment'
+    },
+    {
+        name: 'Sunrise Senior Living Center',
+        url: '',
+        description: 'A residential community that provides specialized care and services for elderly residents'
+    },
+    {
+        name: 'Fort Bend Library',
+        url: '',
+        description: "Public library featuring a children's section, study areas, and computers while offering several volunteer opportunities"
+    },
+    {
+        name: 'Katy Christian Ministries',
+        url: '',
+        description: 'Nonprofit that offers emergency financial assistance and resiliency programs, a Food Pantry dedicated to eradicating hunger, a Crisis Center that houses the Domestic Abuse Center and the Sexual Assault Center, and Resale Stores / Donation Center'
+    }
+];
