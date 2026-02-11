35858caab5da3b4cccd292cfb676659103e21de4
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-11 16:01:55 +0000

    Added markets to database of cultural resources

--- src/cultural.js
+++ src/cultural.js
@@ -1,1 +1,70 @@
 /*** CULTURAL ***/
+
+// Database for cultural resources
+const markets = [
+    {
+        name: 'Arbat Market',
+        url: '',
+        description: 'Eastern and Russian grocery store that specializes in imported goods such as cheeses, meats, bread, and unique produce and offers a wide variety of products from fresh produce to frozen meals and snacks'
+    },
+    {
+        name: 'The Best Stop Cajun Market',
+        url: '',
+        description: 'Grocery store that specializes in cajun meats and cuisine, such as bondin, crackling, and crawfish pie'
+    },
+    {
+        name: 'H-Mart',
+        url: '',
+        description: 'Largest Asian supermarket chain that specializes in Korean, Japanese, Chinese, and other Asian groceries, produce, seafood, snacks, household items, and skincare'
+    },
+    {
+        name: 'Fiesta Mart',
+        url: '',
+        description: 'Latin American supermarket chain with a diverse selection of produce, bakery goods, meats, seafood, etc'
+    },
+    {
+        name: 'Tinda Filipino Store',
+        url: '',
+        description: 'Filipino grocery store with specialty food'
+    },
+    {
+        name: 'Triveni Supermarket',
+        url: '',
+        description: 'South Asian grocery store known for offering authentic Indian spieces, fresh produce, specialty items, halal meats, a bakery, and a food court with Indian meals'
+    },
+    {
+        name: 'Indian Bazaar and Cafe',
+        url: '',
+        description: 'Casual, welcoming cafe and grocery store for Indian staples including spices, lentils, and snacks'
+    },
+    {
+        name: 'Keemat Grocers',
+        url: '',
+        description: 'Indian groceries and spices market also stocking fresh produce, dried foods and snacks'
+    },
+    {
+        name: 'Vishala Grocery',
+        url: '',
+        description: 'Offers snacks, dairy, and frozen goods stocked at a store with an eatery specializing in Indian groceries'
+    },
+    {
+        name: 'Triumphant Food Store',
+        url: '',
+        description: 'Supermarket specializing in traditional meals and products from Africa'
+    },
+    {
+        name: 'Tawakkul Grocers and Halal Meat',
+        url: '',
+        description: 'South Asian/Desi grocery store and butcher shop known for its excellent quality fresh, halal meat (chicken, goat, steak), diverse Asian groceries, and delicious homemade prepared foods like samosas and biryani'
+    },
+    {
+        name: 'Daiso',
+        url: '',
+        description: 'Japanese retail chain known for its vast selection of unique and affordable household goods, stationary, beauty products, snacks, and quirky lifestyle items'
+    },
+    {
+        name: '99 Ranch',
+        url: '',
+        description: 'Supermarket chain known for a vast selection of East and Southeast Asian groceries, fresh produce, live seafood, meats, bakery items, prepared foods, and specialty products'
+    }
+];
