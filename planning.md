cd18d103474ab734fe1d384943a23f77f7198368
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-11 20:29:09 +0000

    Added local businesses to cultural database

--- planning.md
+++ planning.md
@@ -2,9 +2,11 @@
 
 ## All New Resources
 - Fix placement of descriptions (starting with worship places in cultural)
-- Ensure i is fixed for all ids (i + length of previous subcategory - 1 + length of other previous subcategory - 1...), subtracting 1 from each length
+    - Ensure i is fixed for all ids (i + length of previous subcategory - 1 + length of other previous subcategory - 1...), subtracting 1 from each length
+    - Only 1st resource of each subcategory (except cultural markets) is wrong?
 - Add urls to databases
 - Add site images to assets
+- Add each category to map
 
 ## All Old Resources
 - Add subcategory headings
---
+++ src/businesses.js
@@ -0,0 +1,72 @@
+/*** SMALL BUSINESSES ***/
+
+// Database for small business resources
+const local = [
+    {
+        name: 'Farmers Market On Grand Parkway',
+        url: '',
+        description: 'Small outdoor market selling organic and local produce, eggs, honey, and baked goods along with a wide selection of artisan food and goods'
+    },
+    {
+        name:'Cobblestone Cottage',
+        url: '',
+        description: 'Local family-owned gift and decor boutique'
+    },
+    {
+        name: 'Loves Dumpling House',
+        url: '',
+        description: 'Family-run Chinese dumpling and noodle house'
+    },
+    {
+        name: 'Katy Printers',
+        url: '',
+        description: 'Friendly, family-owned print shop turning out business cards, brochures, envelopes, and banners'
+    },
+    {
+        name: 'Mockingbird Books',
+        url: '',
+        description: 'Cozy, independent bookstore focused on building community through books, coffee, wine, and events'
+    },
+    {
+        name: 'Painted Tree Boutiques',
+        url: '',
+        description: 'A multi-vendor retail marketplace where numerous small businesses and local artisans rent individual booths to sell unique goods like clothing, home decor, jewelry, and gifts'
+    },
+    {
+        name: 'Chilled Cryospa',
+        url: '',
+        description: 'A wellness center offering cryotherapy and holistic therapies like red light therapy, compression therapy, chiropractic care, and IV hydration'
+    },
+    {
+        name: 'Local Table',
+        url: '',
+        description: 'Family-owned restaurant chain known for fresh, scratch-made American cuisine in a family-friendly yet upscale setting'
+    },
+    {
+        name: 'Teal Poppy',
+        url: '',
+        description: "Women's boutique known for cute, hand-picked clothing acccessories, and gifts"
+    },
+    {
+        name: 'Buzz and Bites',
+        url: '',
+        description: 'Family-owned restaurant serving burgers, waffles, croissants, and wine'
+    },
+    {
+        name: 'Steam Dumpling',
+        url: '',
+        description: 'Chinese restaurant known for its variety of freshly made, authentic dumplings and other Chinese specialties'
+    },
+    {
+        name: "What's Poppin Popcorn",
+        url: '',
+        description: 'Small, local popcorn store selling a large selection of popcorn in sweet, savory, and creative flavors'
+    },
+    {
+        name: 'The Refinery Market',
+        url: '',
+        description: 'A family-friendly community market, featuring local vendors, food trucks, live music, and family activities like face painting'
+    }
+];
+const help = [];
+const places = [];
