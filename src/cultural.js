efe248b342c9bb403b3b358817f0639e4b7d3d16
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-11 19:54:35 +0000

    Attempted to fix description placement for worship places

--- src/cultural.js
+++ src/cultural.js
@@ -209,24 +209,24 @@ for (let i = 0; i < worship.length; i++) {
         <div class="resource">
             <a href="${r.url}" target="_blank">
                 <div class="card">
-                    <div id="text${i}" class="text">
+                    <div id="text${i + markets.length - 1}" class="text">
                         <h3>${r.name}</h3>
                         <p>${r.description}</p>
                     </div>
-                    <div id="site-img${i}" class="site-img"></div>
+                    <div id="site-img${i + markets.length - 1}" class="site-img"></div>
                 </div>
             </a>
         </div>
     `;
 
     // Add image of resource site below its name and description
-    document.querySelector(`#site-img${i}`).style.setProperty('background', `url("assets/img/sites/cultural/${i}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
+    document.querySelector(`#site-img${i + markets.length - 1}`).style.setProperty('background', `url("assets/img/sites/cultural/${i + markets.length - 1}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
 
     // Access div containing current name and description
-    let text = document.querySelector(`#text${i}`);
+    let text = document.querySelector(`#text${i + markets.length - 1}`);
 
     // Adjust height of resource site image based on overall size of name and description
-    document.querySelector(`#site-img${i}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
+    document.querySelector(`#site-img${i + markets.length - 1}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
 }
 // Fix description placement?
 
