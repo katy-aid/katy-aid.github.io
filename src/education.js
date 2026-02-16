af9d135e0dfe4edce0a768bd69e34259d6ff2072
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-16 17:35:40 -0600

    Restructured education database

--- src/education.js
+++ src/education.js
@@ -1,213 +1,175 @@
 /*** EDUCATION ***/
 
-// Database for education resources
-const tutoring = [
-    {
-        name: 'Fort Bend Public Libraries',
-        url: 'https://www.fortbendlibraries.gov/',
-        description: 'Offers free books, TV, movies, and computer access'
-    },
-    {
-        name: 'Compudopt',
-        url: 'https://www.compudopt.org/',
-        description: 'Offers free computers and technology to qualifying students'
-    },
-    {
-        name: 'KCM Homework Buddies',
-        url: 'https://ktcm.org/event/homework-buddies/',
-        description: 'Offers free tutoring for grades K-12 in the Katy area'
-    },
-    {
-        name: 'The Tutoring Center',
-        url: 'http://tutoringcenter.com/center/katytx',
-        description: 'Personalized tutoring and study skill support for students of all ages'
-    },
-    {
-        name: 'Solutions Education',
-        url: 'https://www.sol-edu.com/',
-        description: 'Private tutoring and test preparation center that provides personalized academic support for students in elementary to high school'
-    },
-    {
-        name: "Counter's Tutoring Connection",
-        url: 'https://counterstutoring.com/',
-        description: 'Private tutoring center focused on personalized education for students preschool through college'
-    },
-    {
-        name: 'Katy Scholars',
-        url: 'https://maps.app.goo.gl/yeGkPcaQnALB72qe7',
-        description: 'Educational services business, specializing in tutoring, test preparation, and academic support'
-    },
-    {
-        name: 'Benedictine Tutors',
-        url: 'https://benedictinetutors.com/',
-        description: 'Tutoring and test preparation center, serving students grades K-12'
-    },
-    {
-        name: 'Best in Class Education Center',
-        url: 'http://bestinclasseducation.com/',
-        description: 'Supplemental learning center that provides personalized academic enrichment tutoring for kids in all grade levels'
-    },
-    {
-        name: 'Mathnasium',
-        url: 'https://www.mathnasium.com/math-centers/cincoranch',
-        description: 'Math-focused learning center, offering individualized tutoring for students from preschool through college'
-    },
-    {
-        name: 'Kumon Math and Reading Center',
-        url: 'https://www.kumon.com/katy-cinco-ranch-southwest/?utm_source=local&utm_medium=gmb&utm_campaign=Katy%20Cinco%20Ranch%20Southwest',
-        description: 'Provides after-school tutoring in math and reading for children from preschool through high school'
-    },
-    {
-        name: 'C2 Education',
-        url: 'https://www.c2educate.com/locations/grand-parkway-katy-tx/?utm_source=google&utm_medium=local&utm_campaign=localmaps',
-        description: 'Tutoring and college preparation center specializing in personalized academic tutoring, standardized testing preparation, and college admission counseling for students in grades K-12'
-    },
-    {
-        name: 'Tutoring Club of Katy',
-        url: 'https://tutoringclub.com/katytx/',
-        description: 'Supplemental education center providing individualized tutoring for students preschool through 12th grade'
-    },
-    {
-        name: 'Katy Test Prep and More',
-        url: 'http://katytestprep.com/',
-        description: 'Education center focused on standardized test preparation and academic tutoring'
-    },
-    {
-        name: 'Huntington Learning Center',
-        url: 'https://huntingtonhelps.com/locations/katy-tx?utm_medium=organic&utm_campaign=localmaps',
-        description: 'Learning center that offers tutoring and test preparation services'
-    }
-];
-const esl = [
-    {
-        name: '123AmericanEnglish',
-        url: 'http://www.123americanenglish.com/',
-        description: 'Private language academy that specializes in teaching English as a second language to speakers of other languages by offering flexible course options'
-    },
-    {
-        name: 'Crossing Borders Language Center',
-        url: 'https://cblckaty.com/',
-        description: 'Private language school that provides personalized language instruction for adults, teens, and children'
-    },
-    {
-        name: 'Katy ISD Parent ESL Classes',
-        url: 'https://maps.app.goo.gl/iuPCRcnt8rwcnTyd7',
-        description: 'Provides free adult English classes along with mentoring and workshops'
-    },
-    {
-        name: 'Fort Bend Library',
-        url: 'https://www.fortbendlibraries.gov/locations-hours/cinco-ranch',
-        description: 'Periodically hosts ESL conversation circles, informal free English practice events where learners can improve their speaking skills'
-    },
-    {
-        name: 'Trinity Baptist Church',
-        url: 'https://trinity-katy.org/esl-english-classes/',
-        description: 'Offers free ESL classes for adults, beginner through advanced'
-    },
-    {
-        name: 'Christ Church',
-        url: 'https://www.cckpca.org/esl',
-        description: 'Offers ESL classes with multiple proficiency levels'
-    },
-    {
-        name: 'St. Bartholomew Catholic Church',
-        url: 'https://st-bart.org/esl',
-        description: 'Offers morning and evening ESL classes'
-    }
-];
-const adult = [
-    {
-        name: 'Katy ISD Community Education',
-        url: 'https://www.katyisd.org/community/community-education/home',
-        description: 'Offers non-credit adult education courses virtually in topics like computer, language, wellness, arts, and career skills'
-    },
-    {
-        name: 'Lone Star College-Cypress Center',
-        url: 'http://www.lonestar.edu/cypresscenter.htm',
-        description: 'Community college that offers a wide range of adult education and workforce training programs'
-    }
-]
+// Database for all education resources
+const database = {
+    category: 'education',
+    data: [
+        {
+            title: 'Tutoring Help',
+            resources: [
+                {
+                    name: 'Fort Bend Public Libraries',
+                    url: 'https://www.fortbendlibraries.gov/',
+                    description: 'Offers free books, TV, movies, and computer access'
+                },
+                {
+                    name: 'Compudopt',
+                    url: 'https://www.compudopt.org/',
+                    description: 'Offers free computers and technology to qualifying students'
+                },
+                {
+                    name: 'KCM Homework Buddies',
+                    url: 'https://ktcm.org/event/homework-buddies/',
+                    description: 'Offers free tutoring for grades K-12 in the Katy area'
+                },
+                {
+                    name: 'The Tutoring Center',
+                    url: 'http://tutoringcenter.com/center/katytx',
+                    description: 'Personalized tutoring and study skill support for students of all ages'
+                },
+                {
+                    name: 'Solutions Education',
+                    url: 'https://www.sol-edu.com/',
+                    description: 'Private tutoring and test preparation center that provides personalized academic support for students in elementary to high school'
+                },
+                {
+                    name: "Counter's Tutoring Connection",
+                    url: 'https://counterstutoring.com/',
+                    description: 'Private tutoring center focused on personalized education for students preschool through college'
+                },
+                {
+                    name: 'Katy Scholars',
+                    url: 'https://maps.app.goo.gl/yeGkPcaQnALB72qe7',
+                    description: 'Educational services business, specializing in tutoring, test preparation, and academic support'
+                },
+                {
+                    name: 'Benedictine Tutors',
+                    url: 'https://benedictinetutors.com/',
+                    description: 'Tutoring and test preparation center, serving students grades K-12'
+                },
+                {
+                    name: 'Best in Class Education Center',
+                    url: 'http://bestinclasseducation.com/',
+                    description: 'Supplemental learning center that provides personalized academic enrichment tutoring for kids in all grade levels'
+                },
+                {
+                    name: 'Mathnasium',
+                    url: 'https://www.mathnasium.com/math-centers/cincoranch',
+                    description: 'Math-focused learning center, offering individualized tutoring for students from preschool through college'
+                },
+                {
+                    name: 'Kumon Math and Reading Center',
+                    url: 'https://www.kumon.com/katy-cinco-ranch-southwest/?utm_source=local&utm_medium=gmb&utm_campaign=Katy%20Cinco%20Ranch%20Southwest',
+                    description: 'Provides after-school tutoring in math and reading for children from preschool through high school'
+                },
+                {
+                    name: 'C2 Education',
+                    url: 'https://www.c2educate.com/locations/grand-parkway-katy-tx/?utm_source=google&utm_medium=local&utm_campaign=localmaps',
+                    description: 'Tutoring and college preparation center specializing in personalized academic tutoring, standardized testing preparation, and college admission counseling for students in grades K-12'
+                },
+                {
+                    name: 'Tutoring Club of Katy',
+                    url: 'https://tutoringclub.com/katytx/',
+                    description: 'Supplemental education center providing individualized tutoring for students preschool through 12th grade'
+                },
+                {
+                    name: 'Katy Test Prep and More',
+                    url: 'http://katytestprep.com/',
+                    description: 'Education center focused on standardized test preparation and academic tutoring'
+                },
+                {
+                    name: 'Huntington Learning Center',
+                    url: 'https://huntingtonhelps.com/locations/katy-tx?utm_medium=organic&utm_campaign=localmaps',
+                    description: 'Learning center that offers tutoring and test preparation services'
+                }
+            ]
+        },
+        {
+            title: 'English as a Second Language (ESL)',
+            resources: [
+                {
+                    name: '123AmericanEnglish',
+                    url: 'http://www.123americanenglish.com/',
+                    description: 'Private language academy that specializes in teaching English as a second language to speakers of other languages by offering flexible course options'
+                },
+                {
+                    name: 'Crossing Borders Language Center',
+                    url: 'https://cblckaty.com/',
+                    description: 'Private language school that provides personalized language instruction for adults, teens, and children'
+                },
+                {
+                    name: 'Katy ISD Parent ESL Classes',
+                    url: 'https://maps.app.goo.gl/iuPCRcnt8rwcnTyd7',
+                    description: 'Provides free adult English classes along with mentoring and workshops'
+                },
+                {
+                    name: 'Fort Bend Library',
+                    url: 'https://www.fortbendlibraries.gov/locations-hours/cinco-ranch',
+                    description: 'Periodically hosts ESL conversation circles, informal free English practice events where learners can improve their speaking skills'
+                },
+                {
+                    name: 'Trinity Baptist Church',
+                    url: 'https://trinity-katy.org/esl-english-classes/',
+                    description: 'Offers free ESL classes for adults, beginner through advanced'
+                },
+                {
+                    name: 'Christ Church',
+                    url: 'https://www.cckpca.org/esl',
+                    description: 'Offers ESL classes with multiple proficiency levels'
+                },
+                {
+                    name: 'St. Bartholomew Catholic Church',
+                    url: 'https://st-bart.org/esl',
+                    description: 'Offers morning and evening ESL classes'
+                }
+            ]
+        },
+        {
+            title: 'Adult Education',
+            resources: [
+                {
+                    name: 'Katy ISD Community Education',
+                    url: 'https://www.katyisd.org/community/community-education/home',
+                    description: 'Offers non-credit adult education courses virtually in topics like computer, language, wellness, arts, and career skills'
+                },
+                {
+                    name: 'Lone Star College-Cypress Center',
+                    url: 'http://www.lonestar.edu/cypresscenter.htm',
+                    description: 'Community college that offers a wide range of adult education and workforce training programs'
+                }
+            ]
+        }
+    ]
+};
 
-// Populate page with tutoring help
-document.querySelector(`section.education`).innerHTML += `<h2 class="subcategory">Tutoring Help</h2>`;
-for (let i = 0; i < tutoring.length; i++) {
-    let r = tutoring[i];
-    document.querySelector(`section.education`).innerHTML += `
-        <div class="resource">
-            <a href="${r.url}" target="_blank">
-                <div class="card">
-                    <div id="text${i}" class="text">
-                        <h3>${r.name}</h3>
-                        <p>${r.description}</p>
+// Populate page with all medical resources
+let cat = database.category;
+for (let s = 0; s < database.data.length; s++) { // Iterates through each subcategory object within database
+    let subcat = database.data[s];
+    document.querySelector(`section.${cat}`).innerHTML += `<h2 class="subcategory">${subcat.title}</h2>`;
+    for (let i = 0; i < subcat.resources.length; i++) { // Iterates through each resource within subcategory
+        let r = subcat.resources[i];
+        document.querySelector(`section.${cat}`).innerHTML += `
+            <div class="resource">
+                <a href="${r.url}" target="_blank">
+                    <div class="card">
+                        <div id="text-${s}-${i}" class="text">
+                            <h3>${r.name}</h3>
+                            <p>${r.description}</h3>
+                        </div>
+                        <div id="site-img-${s}-${i}" class="site-img"></div>
                     </div>
-                    <div id="site-img${i}" class="site-img"></div>
-                </div>
-            </a>
-        </div>
-    `;
+                </a>
+            </div>
+        `;
 
-    // Add image of resource site below its name and description
-    document.querySelector(`#site-img${i}`).style.setProperty('background', `url("assets/img/sites/education/${i}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
+        // Add image of resource site below its name and description
+        document.querySelector(`#site-img-${s}-${i}`).style.setProperty('background', `url("assets/img/sites/${cat}/${subcat.title}/${i}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
 
-    // Access div containing current name and description
-    let text = document.querySelector(`#text${i}`);
+        // Access div containing current name and description
+        // let text = document.querySelector(`#text-${s}-${i}`);
 
-    // Adjust height of resource site image based on overall size of name and description
-    document.querySelector(`#site-img${i}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
-}
-
-// Populate page with ESL help
-document.querySelector(`section.education`).innerHTML += `<h2 class="subcategory">English as a Second Language (ESL)</h2>`;
-for (let i = 0; i < esl.length; i++) {
-    let r = esl[i];
-    document.querySelector(`section.education`).innerHTML += `
-        <div class="resource">
-            <a href="${r.url}" target="_blank">
-                <div class="card">
-                    <div id="text${i + tutoring.length}" class="text">
-                        <h3>${r.name}</h3>
-                        <p>${r.description}</h3>
-                    </div>
-                    <div id="site-img${i + tutoring.length}" class="site-img"></div>
-                </div>
-            </a>
-        </div>
-    `;
-
-    // Add image of resource site below its name and description
-    document.querySelector(`#site-img${i + tutoring.length}`).style.setProperty('background', `url("assets/img/sites/education/${i + tutoring.length}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
-
-    // Access div containing current name and description
-    let text = document.querySelector(`#text${i + tutoring.length}`);
-
-    // Adjust height of resource site image based on overall size of name and description
-    document.querySelector(`#site-img${i + tutoring.length}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
-}
-
-// Populate page with adult education resources
-document.querySelector(`section.education`).innerHTML += `<h2 class="subcategory">Adult Education</h2>`;
-for (let i = 0; i < adult.length; i++) {
-    let r = adult[i];
-    document.querySelector(`section.education`).innerHTML += `
-        <div class="resource">
-            <a href="${r.url}" target="_blank">
-                <div class="card">
-                    <div id="text${i + tutoring.length + esl.length}" class="text">
-                        <h3>${r.name}</h3>
-                        <p>${r.description}</p>
-                    </div>
-                    <div id="site-img${i + tutoring.length + esl.length}" class="site-img"></div>
-                </div>
-            </a>
-        </div>
-    `;
-
-    // Add image of resource site below its name and description
-    document.querySelector(`#site-img${i + tutoring.length + esl.length}`).style.setProperty('background', `url("assets/img/sites/education/${i + tutoring.length + esl.length}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
-
-    // Access div containing current name and description
-    let text = document.querySelector(`#text${i + tutoring.length + esl.length}`);
-
-    // Adjust height of resource site image based on overall size of name and description
-    document.querySelector(`#site-img${i + tutoring.length + esl.length}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
+        // Adjust height of resource site image based on overall size of name and description
+        // document.querySelector(`#site-img-${s}-${i}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
+    }
 }
