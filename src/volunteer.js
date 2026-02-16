23fa709e02186374e5c7fb5000b76e2a0b45d4e9
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-16 12:00:29 -0600

    Added urls for volunteer opportunities

--- src/volunteer.js
+++ src/volunteer.js
@@ -4,57 +4,57 @@
 const opportunities = [
     {
         name: 'Ballard House',
-        url: '',
+        url: 'https://www.theballardhouse.org',
         description: 'Offers no-cost temporary housing for individuals and caregivers undergoing treatment'
     },
     {
         name: 'Sunrise Senior Living Center',
-        url: '',
+        url: 'https://www.sunriseseniorliving.com/communities/tx/sunrise-of-cinco-ranch',
         description: 'A residential community that provides specialized care and services for elderly residents'
     },
     {
         name: 'Fort Bend Library',
-        url: '',
+        url: 'https://www.fortbendlibraries.gov/locations-hours/cinco-ranch',
         description: "Public library featuring a children's section, study areas, and computers while offering several volunteer opportunities"
     },
     {
         name: 'Katy Christian Ministries',
-        url: '',
+        url: 'https://ktcm.org/',
         description: 'Nonprofit that offers emergency financial assistance and resiliency programs'
     },
     {
         name: 'Christ Clinic',
-        url: '',
+        url: 'http://www.christclinickaty.org/',
         description: 'Nonprofit that offers high quality healthcare and health-related services to those who are uninsured or underinsured'
     },
     {
         name: 'Memorial Hermann Katy Hospital',
-        url: '',
+        url: 'https://memorialhermann.org/locations/katy',
         description: 'Provides clinical expertise, patient-centered care, and leading-edge technology'
     },
     {
         name: "Cinco Ranch Alzheimer's Special Care Center",
-        url: '',
+        url: 'https://sinceriseniorliving.com/cinco-ranch/',
         description: "Retirement community dedicated to providing memory care in a secure, supportive environment designed for individuals living with Alzheimer's and dementia"
     },
     {
         name: 'Legacy at Falcon Point',
-        url: '',
+        url: 'https://www.pegasusseniorliving.com/the-legacy-at-falcon-point-in-katy/',
         description: 'Assisted living and memory care community providing personalized support with daily activities such as line dancing and yoga'
     },
     {
         name: 'Keep Encouraging Youth toward Success (KEYS) Mentoring',
-        url: '',
+        url: 'https://www.katyisd.org/partners-in-education/programs/keys-mentoring',
         description: 'Katy ISD program that primarily focusees on relationship building, rather than tutoring between a mentor and student'
     },
     {
         name: 'Katy Heritage Society',
-        url: '',
+        url: 'https://www.katyheritagesociety.com/',
         description: 'Nonprofit dedicated to furthering the educational and cultural development of the community by preserving, restoring, and displaying historical landmarks, natural beauty, documents'
     },
     {
-        name: "Krause Children's Center",
-        url: '',
+        name: "Upbring Krause Children's Center",
+        url: 'https://maps.app.goo.gl/rfAE6ggdXiGeiDb5A',
         description: 'Offers a nurturing and temporary haven for hundreds of unaccommpanied children annually, providing them with solace, safety, and a pathway to healing after their journey across the Souther border'
     }
 ];
