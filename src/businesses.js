cd5bc6108f10e3204051fe6db969972c3b80286a
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-16 11:50:59 -0600

    Added urls for local and family run businesses

--- src/businesses.js
+++ src/businesses.js
@@ -4,67 +4,67 @@
 const local = [
     {
         name: 'Farmers Market On Grand Parkway',
-        url: '',
+        url: 'http://www.farmersmarketongrandparkway.com/',
         description: 'Small outdoor market selling organic and local produce, eggs, honey, and baked goods along with a wide selection of artisan food and goods'
     },
     {
         name:'Cobblestone Cottage',
-        url: '',
+        url: 'https://shopcobblestonecottage.com/',
         description: 'Local family-owned gift and decor boutique'
     },
     {
         name: 'Loves Dumpling House',
-        url: '',
+        url: 'https://www.lovesdumplinghouse.com/',
         description: 'Family-run Chinese dumpling and noodle house'
     },
     {
         name: 'Katy Printers',
-        url: '',
+        url: 'https://ktprinters.com/',
         description: 'Friendly, family-owned print shop turning out business cards, brochures, envelopes, and banners'
     },
     {
         name: 'Mockingbird Books',
-        url: '',
+        url: 'https://mockingbirdbookskaty.com/',
         description: 'Cozy, independent bookstore focused on building community through books, coffee, wine, and events'
     },
     {
         name: 'Painted Tree Boutiques',
-        url: '',
+        url: 'https://paintedtree.com/',
         description: 'A multi-vendor retail marketplace where numerous small businesses and local artisans rent individual booths to sell unique goods like clothing, home decor, jewelry, and gifts'
     },
     {
         name: 'Chilled Cryospa',
-        url: '',
+        url: 'https://cryospakaty.com/',
         description: 'A wellness center offering cryotherapy and holistic therapies like red light therapy, compression therapy, chiropractic care, and IV hydration'
     },
     {
         name: 'Local Table',
-        url: '',
+        url: 'https://eatatlocaltable.com/locations/cinco-ranch/',
         description: 'Family-owned restaurant chain known for fresh, scratch-made American cuisine in a family-friendly yet upscale setting'
     },
     {
         name: 'Teal Poppy',
-        url: '',
+        url: 'https://tealpoppy.com/?srsltid=AfmBOorjYYZ6ZU6Cv2yqwf3MrgG-GTG8H9x7YXEkbGhpwxTKiEZImGQJ',
         description: "Women's boutique known for cute, hand-picked clothing acccessories, and gifts"
     },
     {
         name: 'Buzz and Bites',
-        url: '',
+        url: 'http://www.buzzandbites.com/',
         description: 'Family-owned restaurant serving burgers, waffles, croissants, and wine'
     },
     {
         name: 'Steam Dumpling',
-        url: '',
+        url: 'https://chowbusmkt.wixsite.com/website-275',
         description: 'Chinese restaurant known for its variety of freshly made, authentic dumplings and other Chinese specialties'
     },
     {
         name: "What's Poppin Popcorn",
-        url: '',
+        url: 'https://whatspoppinpopcorn.co/',
         description: 'Small, local popcorn store selling a large selection of popcorn in sweet, savory, and creative flavors'
     },
     {
         name: 'The Refinery Market',
-        url: '',
+        url: 'https://www.therefinerykaty.com/',
         description: 'A family-friendly community market, featuring local vendors, food trucks, live music, and family activities like face painting'
     }
 ];
