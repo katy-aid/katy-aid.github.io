a452f2f92cf104f56267ad74cda9af266a38e710
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-19 15:49:19 +0000

    Started adding recreational activities to database

---
+++ src/recreational.js
@@ -0,0 +1,20 @@
+const database = {
+    category: 'recreational',
+    data: [
+        {
+            title: 'Parks',
+            resources: [
+                {
+                    name: 'Mary Jo Peckham Park',
+                    url: 'https://cp4.harriscountytx.gov/Explore/Parks/Precinct-4-Parks/mary-jo-peckham-park',
+                    description: 'An amazing opportunity for fresh-air for the whole family, providing an amazing pond view with gym amenities and a playground'
+                },
+                {
+                    name: 'Willow Fork Park',
+                    url: '',
+                    description: ''
+                }
+            ]
+        }
+    ]
+};
