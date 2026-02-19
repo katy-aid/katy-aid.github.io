7abdc1b193aff9a92748dae7779f2cfdd6c0cf97
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-19 18:44:50 +0000

    Trying to populate recreational page

--- src/recreational.js
+++ src/recreational.js
@@ -1,3 +1,6 @@
+/*** RECREATIONAL ACTIVITIES ***/
+
+// Database for all recreational activities
 const database = {
     category: 'recreational',
     data: [
@@ -43,3 +46,35 @@ const database = {
         }
     ]
 };
+
+// Populate page with all recreational activities
+let cat = database.category;
+for (let s = 0; s < database.data.length; s++) { // Iterates through each subcategory object within database
+    let subcat = database.data[s];
+    document.querySelector(`section.${cat}`).innerHTML += `<h2 class="subcategory">${subcat.title}</h2>`;
+    for (let i = 0; i < subcat.resources.lenggth; i++) { // Iterates through each resource within subcategory
+        let r = subcat.resources[i];
+        document.querySelector(`section.${cat}`).innerHTML += `
+            <div class="resource">
+                <a href="${r.url}" target="_blank">
+                    <div class="card">
+                        <div id="text-${s}-${i}" class="text">
+                            <h3>${r.name}</h3>
+                            <p>${r.description}</h3>
+                        </div>
+                        <div id="site-img-${s}-${i}" class="site-img"></div>
+                    </div>
+                </a>
+            </div>
+        `;
+
+        // Add image of resource site below its name and description
+        document.querySelector(`#site-img-${s}-${i}`).style.setProperty('background', `url("assets/img/sites/${cat}/${subcat.title}/${i}.png") no-repeat top center / cover`, 'important'); // background: imageURL repeat verticalPosition horizontalPosition / size
+
+        // Access div containing current name and description
+        // let text = document.querySelector(`#text-${s}-${i}`);
+
+        // Adjust height of resource site image based on overall size of name and description
+        // document.querySelector(`#site-img-${s}-${i}`).style.setProperty('height', `calc(100% - 20px - ${window.getComputedStyle(text).height})`);
+    }
+}
