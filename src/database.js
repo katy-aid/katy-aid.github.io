b421900b7202ba315a57580a1eb66112ba089064
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2026-02-20 12:47:09 -0600

    Optimized recreational database

--- src/database.js
+++ src/database.js
@@ -548,6 +548,48 @@ const database = {
                 }
             ]
         }
+    ],
+    recreational: [
+        {
+            title: 'Parks',
+            resources: [
+                {
+                    name: 'Mary Jo Peckham Park',
+                    url: 'https://cp4.harriscountytx.gov/Explore/Parks/Precinct-4-Parks/mary-jo-peckham-park',
+                    description: 'An amazing opportunity for fresh-air for the whole family, providing an amazing pond view with gym amenities and a playground'
+                },
+                {
+                    name: 'Willow Fork Park',
+                    url: 'https://www.willowforkdrainagedistrict.com/parks-trails/willow-fork-park/',
+                    description: 'Offers a vast area of exploration with a butterfly garden, ultimate frisbee courses, and an expansive walkway that provides the perfect opportunity for a day spent outside'
+                },
+                {
+                    name: 'Typhoon Texas',
+                    url: 'https://typhoontexas.com/houston/',
+                    description: "An accessible waterpark for the whole family to enjoy a hot summer's day with an inexpensive ticket and time spent with loved ones"
+                }
+            ]
+        },
+        {
+            title: 'Shopping Centers',
+            resources: [
+                {
+                    name: 'Katy Mills Mall',
+                    url: 'https://www.simon.com/mall/katy-mills',
+                    description: 'A conveniently located shopping center, perfect for day-to-day essentials while also providing a fun outing as a family'
+                },
+                {
+                    name: 'La Centerra',
+                    url: 'https://lacenterra.com/',
+                    description: 'A beautiful outdoor shopping center and the ideal place to do upscale shopping, dining, and entertainment within the Katy area'
+                },
+                {
+                    name: 'Katy Asia Town',
+                    url: 'https://katyasiantown.com/en/5-2/',
+                    description: 'A vibrant, culture-rich shopping center in the Katy Area'
+                }
+            ]
+        }
     ]
 };
 
